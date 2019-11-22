a38e427159b808c2702a538e753b4106c04ad799
CodingCCarpenter <christineassists@aol.com>
2019-11-21 20:23:53 -0600

    test

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -6,10 +6,8 @@ const AddedFeature = props => {
       {/* Add an onClick to run a function to remove a feature */}
       <button 
         className='button'
-        onClick = {() => {
-          props.removeFeature(props.feature)
-        }}
-      > X
+        onClick = {() => {props.removeFeature(props.feature)}}>
+           X
       </button>
       {props.feature.name}
     </li>
